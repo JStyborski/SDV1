557c5e098522de7c88c3759980d043d397764a98
JStyborski <49502351+JStyborski@users.noreply.github.com>
2024-10-30 20:29:19 +0800

    Improved img2img
    - Better logic flow for saving streamlines
    - Implements eta value and CFG scale in img2noise2img
    - Allows different CFG scales on forward/reverse img2img
    - Allows different eta values for forward/reverse img2img

--- scripts/JS_img2img.py
+++ scripts/JS_img2img.py
@@ -66,28 +66,30 @@ def arg_inputs():
     parser.add_argument('--outdir', default='outputs/img2img-samples', type=str, nargs='?', help='dir to write results to')
     parser.add_argument('--skip_grid', default=False, type=lambda x: bool(strtobool(x)), help='do not save a grid, only individual samples. Helpful when evaluating lots of samples')
     parser.add_argument('--skip_save', default=False, type=lambda x: bool(strtobool(x)), help='Do not save indiviual samples. For speed measurements.')
-    parser.add_argument('--ddim_steps', default=50, type=int, help='Number of ddim sampling steps (across T=1000 DDPM steps).')
-    parser.add_argument('--sampler', default='ddim', choices=['ddim', 'plms', 'dpm'], type=str, help='Solver type.')
     parser.add_argument('--fixed_code', default=False, type=lambda x: bool(strtobool(x)), help='If enabled, uses the same starting code across all samples.')
-    parser.add_argument('--ddim_eta', default=0.0, type=float, help='DDIM eta (eta=0.0 is deterministic sampling).')
-    parser.add_argument('--n_iter', default=1, type=int, help='sample this often')
+    parser.add_argument('--vae_sampling', default='random', choices=['random', 'deterministic'], type=str, help='Encoding distribution sampling method - deterministic sets var/std to 0.')
+    parser.add_argument('--use_diffusion', default=True, type=lambda x: bool(strtobool(x)), help='Whether to use diffusion or skip it (VAE only).')
+    parser.add_argument('--sampler', default='ddim', choices=['ddim', 'plms', 'dpm'], type=str, help='Solver type.')
+    parser.add_argument('--ddim_steps', default=50, type=int, help='Number of ddim sampling steps (across T=1000 DDPM steps).')
+    parser.add_argument('--use_orig_tsteps', default=True, type=lambda x: bool(strtobool(x)), help='Whether to use original timestep scheme (wrong) or mine (correct)')
+    parser.add_argument('--ddim_eta_fwd', default=1.0, type=float, help='DDIM eta in image-to-noise (1.0 is random, 0.0 is DDIM inversion).')
+    parser.add_argument('--ddim_eta_rev', default=0.0, type=float, help='DDIM eta in noise-to-image (1.0 is DDPM, 0.0 is DDIM).')
+    parser.add_argument('--n_iter', default=1, type=int, help='Number of sampling runs per image.')
     parser.add_argument('--img_size', default=512, type=int, help='Image size to input to LDM (after resize).')
     parser.add_argument('--C', default=4, type=int, help='Latent channels.')
     parser.add_argument('--f', default=8, type=int, help='Downsampling factor, often 8 or 16')
     parser.add_argument('--n_samples', default=2, type=int, help='How many samples to produce for each given prompt. A.k.a batch size')
     parser.add_argument('--n_rows', default=0, type=int, help='Rows in the grid (default: n_samples)')
-    parser.add_argument('--scale', default=5.0, type=float, help='Unconditional guidance scale: eps = eps(x, empty) + scale * (eps(x, cond) - eps(x, empty))')
+    parser.add_argument('--scale_fwd', default=5.0, type=float, help='CFG scale in forward process: 1.0 corresponds to conditional output, >1.0 emphasizes conditioning.')
+    parser.add_argument('--scale_rev', default=5.0, type=float, help='CFG scale in reverse process: 1.0 corresponds to conditional output, >1.0 emphasizes conditioning.')
     parser.add_argument('--strength', default=0.75, type=float, help='Strength for noising/unnoising. 1.0 corresponds to full destruction of information in init image')
     parser.add_argument('--from-file', type=str, help='If specified, load prompts from this file')
     parser.add_argument('--config', default='./configs/stable-diffusion/v1-inference.yaml', type=str, help='path to config which constructs model')
     parser.add_argument('--ckpt', default='./checkpoints/stable-diffusion-v1-5/v1-5-pruned-emaonly.ckpt', type=str, help='path to checkpoint of model')
     parser.add_argument('--seed', default=42, type=int, help='the seed (for reproducible sampling)')
     parser.add_argument('--precision', default='autocast', choices=['full', 'autocast'], type=str, help='evaluate at this precision')
-    parser.add_argument('--deterministic_vae', default=True, type=lambda x: bool(strtobool(x)), help='Whether to use mean VAE encoding or sample.')
-    parser.add_argument('--deterministic_diff', default=True, type=lambda x: bool(strtobool(x)), help='Whether to use custom DDIM scripts or native stochastic diffusion. Set as None to skip diffusion entirely.')
-    parser.add_argument('--use_orig_tsteps', default=False, type=lambda x: bool(strtobool(x)), help='Whether to use original timestep scheme (wrong) or mine (correct)')
     parser.add_argument('--save_latents', default=False, type=lambda x: bool(strtobool(x)), help='Whether to save out latents.')
-    parser.add_argument('--save_streamlines', default=False, type=lambda x: bool(strtobool(x)), help='If deterministic_diff, whether to save out every step of the latents.')
+    parser.add_argument('--save_streamlines', default=False, type=lambda x: bool(strtobool(x)), help='Whether to save out every step of the diffusion latents.')
     args = parser.parse_args()
     return args
 
@@ -134,11 +136,11 @@ def main():
         os.makedirs(std_path, exist_ok=True)
         init_path = os.path.join(opt.outdir, 'Lat_Init')
         os.makedirs(init_path, exist_ok=True)
-        if opt.deterministic_diff is not None:
-            i2n_path = os.path.join(opt.outdir, 'Lat_I2N')
-            os.makedirs(i2n_path, exist_ok=True)
-            n2i_path = os.path.join(opt.outdir, 'Lat_N2I')
-            os.makedirs(n2i_path, exist_ok=True)
+    if opt.save_streamlines:
+        i2n_path = os.path.join(opt.outdir, 'Lat_I2N')
+        os.makedirs(i2n_path, exist_ok=True)
+        n2i_path = os.path.join(opt.outdir, 'Lat_N2I')
+        os.makedirs(n2i_path, exist_ok=True)
 
     base_count = len(os.listdir(sample_path))
     grid_count = len(os.listdir(outpath)) - 1
@@ -156,7 +158,7 @@ def main():
 
         # Encode input via VAE encoder
         enc_posterior = model.encode_first_stage(init_image)
-        if opt.deterministic_vae:
+        if opt.vae_sampling == 'deterministic':
             # If True, latent always returns posterior mean vector, else samples
             enc_posterior.deterministic = True
             enc_posterior.var = torch.zeros_like(enc_posterior.mean).to(device=enc_posterior.parameters.device)
@@ -168,7 +170,7 @@ def main():
             torch.save(enc_posterior.std.detach().cpu(), os.path.join(std_path, fileName + '_post_std.pt'))
             torch.save(init_latent.detach().cpu(), os.path.join(init_path, fileName + '_init_latent.pt'))
 
-        sampler.make_schedule(ddim_num_steps=opt.ddim_steps, ddim_eta=opt.ddim_eta, verbose=False)
+        sampler.make_schedule(ddim_num_steps=opt.ddim_steps, ddim_eta=opt.ddim_eta_rev, verbose=False)
 
         # Nominal timestepping:
         # ddim_alphas corresponds to timesteps [1, 101, 201, ... 801, 901], ddim_alphas_prev corresponds to [0, 1, 101, ..., 701, 801]
@@ -196,8 +198,7 @@ def main():
             sampler.ddim_alphas = to_torch(model.alphas_cumprod[sampler.ddim_timesteps])
             sampler.ddim_alphas_prev = to_torch(model.alphas_cumprod[sampler.ddim_timesteps_prev])
             sampler.ddim_sqrt_one_minus_alphas = to_torch(torch.sqrt(1 - sampler.ddim_alphas))
-            sampler.ddim_sigmas = to_torch(opt.ddim_eta * torch.sqrt((1 - sampler.ddim_alphas_prev) / (1 - sampler.ddim_alphas)
-                                                                     * (1 - sampler.ddim_alphas / sampler.ddim_alphas_prev)))
+            sampler.ddim_sigmas = to_torch(opt.ddim_eta_rev * torch.sqrt((1 - sampler.ddim_alphas_prev) / (1 - sampler.ddim_alphas) * (1 - sampler.ddim_alphas / sampler.ddim_alphas_prev)))
 
         assert 0. <= opt.strength <= 1., 'Can only work with strength in [0.0, 1.0]'
         assert opt.strength * opt.ddim_steps % 1 == 0, 'To ensure that denoising strength aligns with timestep indexing'
@@ -214,35 +215,37 @@ def main():
 
                             # Get text prompt encodings
                             uc = None
-                            if opt.scale != 1.0:
+                            if opt.scale_fwd != 1.0 or opt.scale_rev != 1.0:
                                 uc = model.get_learned_conditioning(batch_size * [''])
                             if isinstance(prompts, tuple):
                                 prompts = list(prompts)
                             c = model.get_learned_conditioning(prompts)
 
                             # Perform diffusion process
-                            if opt.deterministic_diff is None:
-                                # Skip diffusion process entirely
-                                # The program already does similar since t_enc would be set to 0, but in the DDIM sampler, the 0th alpha value (~0.04)
-                                # still results in significant noise added to the image, which is then not denoised at all
-                                samples = init_latent
-                            elif opt.deterministic_diff:
-                                # Deterministic encode/decode
-                                noise_latent, i2nList = JS_img2noise2img.image_to_noise(init_latent, model, sampler, t_enc, cond=c, uncond=uc, record_latents=opt.save_streamlines)
-                                samples, n2iList = JS_img2noise2img.noise_to_image(noise_latent, model, sampler, t_enc, cond=c, uncond=uc, record_latents=opt.save_streamlines)
-                                if opt.save_latents and opt.save_streamlines:
+                            if opt.use_diffusion and opt.ddim_eta_fwd == 1. and not(opt.save_streamlines):
+                                # Stochastic encode, decode stochasticity determined by opt.ddim_eta_rev
+                                z_enc = sampler.stochastic_encode(init_latent, torch.tensor([t_enc - 1] * batch_size).to(device))
+                                samples = sampler.decode(z_enc, c, t_enc, unconditional_guidance_scale=opt.scale_rev, unconditional_conditioning=uc)
+                                if opt.save_latents:
+                                    torch.save(z_enc.cpu(), os.path.join(i2n_path, fileName + '_i2n_final.pt'))
+                                    torch.save(samples.cpu(), os.path.join(n2i_path, fileName + '_n2i_final.pt'))
+                            elif opt.use_diffusion:
+                                # Encode/decode stochasticity determined by opt.ddim_eta_fwd and opt.ddim_eta_rev
+                                sampler.ddim_sigmas = to_torch(opt.ddim_eta_fwd * torch.sqrt((1 - sampler.ddim_alphas_prev) / (1 - sampler.ddim_alphas) * (1 - sampler.ddim_alphas / sampler.ddim_alphas_prev)))
+                                noise_latent, i2nList = JS_img2noise2img.image_to_noise(init_latent, model, sampler, t_enc, cond=c, uncond=uc, cfg_scale=opt.scale_fwd, record_latents=opt.save_streamlines)
+                                sampler.ddim_sigmas = to_torch(opt.ddim_eta_rev * torch.sqrt((1 - sampler.ddim_alphas_prev) / (1 - sampler.ddim_alphas) * (1 - sampler.ddim_alphas / sampler.ddim_alphas_prev)))
+                                samples, n2iList = JS_img2noise2img.noise_to_image(noise_latent, model, sampler, t_enc, cond=c, uncond=uc, cfg_scale=opt.scale_rev, record_latents=opt.save_streamlines)
+                                if opt.save_streamlines:
                                     torch.save(torch.stack(i2nList), os.path.join(i2n_path, fileName + '_i2n_sl.pt'))
                                     torch.save(torch.stack(n2iList), os.path.join(n2i_path, fileName + '_n2i_sl.pt'))
                                 elif opt.save_latents:
                                     torch.save(noise_latent.cpu(), os.path.join(i2n_path, fileName + '_i2n_final.pt'))
                                     torch.save(samples.cpu(), os.path.join(n2i_path, fileName + '_n2i_final.pt'))
                             else:
-                                # Stochastic encode/decode
-                                z_enc = sampler.stochastic_encode(init_latent, torch.tensor([t_enc - 1] * batch_size).to(device))
-                                samples = sampler.decode(z_enc, c, t_enc, unconditional_guidance_scale=opt.scale, unconditional_conditioning=uc)
-                                if opt.save_latents:
-                                    torch.save(z_enc.cpu(), os.path.join(i2n_path, fileName + '_i2n_final.pt'))
-                                    torch.save(samples.cpu(), os.path.join(n2i_path, fileName + '_n2i_final.pt'))
+                                # Skip diffusion process entirely
+                                # The program already does similar since t_enc would be set to 0, but in the DDIM sampler, the 0th alpha value (~0.04)
+                                # still results in significant noise added to the image, which is then not denoised at all
+                                samples = init_latent
 
                             # Decode samples from latent space
                             x_samples = model.decode_first_stage(samples)
--- scripts/JS_img2noise2img.py
+++ scripts/JS_img2noise2img.py
@@ -1,38 +1,48 @@
 import torch
 
-def image_to_noise(image_batch, model, sampler, t_enc, cond, uncond, record_latents=False):
+def image_to_noise(image_batch, model, sampler, t_enc, cond, uncond, cfg_scale, record_latents=False):
     # Initialize x_t and loop through prediction steps
-    condInput = cond if uncond is None else torch.cat([cond, uncond], dim=0)
-    nRepeat = 1 if uncond is None else 2
+    condInput = cond if uncond is None or cfg_scale == 1. else torch.cat([cond, uncond], dim=0)
     x_t = image_batch
     latentList = [x_t.detach().cpu()] if record_latents else None
     with torch.no_grad():
         for tIdx in range(t_enc):
             t = sampler.ddim_timesteps_prev[tIdx]
-            eps_t = model.apply_model(x_t.repeat(nRepeat, 1, 1, 1), torch.ones(len(image_batch)).cuda() * t, cond=condInput)
-            ab_t = sampler.ddim_alphas_prev[tIdx]        # alpha_bar_t
+            if uncond is None or cfg_scale == 1.:
+                eps_t = model.apply_model(x_t, torch.full([len(x_t)], t, device=x_t.device), condInput)
+            else:
+                eps_t_cond, eps_t_uncond = model.apply_model(torch.cat([x_t] * 2), torch.full([2 * len(x_t)], t, device=x_t.device), condInput).chunk(2)
+                eps_t = eps_t_uncond + cfg_scale * (eps_t_cond - eps_t_uncond)
+            eps = torch.randn_like(eps_t)
+            ab_t = sampler.ddim_alphas_prev[tIdx]        # alpha_bar_{t}
             ab_dt = sampler.ddim_alphas[tIdx]            # alpha_bar_{t+dt}
             a_dt = ab_dt / ab_t                          # Product of alphas between alpha_{t} and alpha_{t+dt}
-            x_dt = a_dt.sqrt() * x_t + ((1 - ab_dt).sqrt() - (a_dt - ab_dt).sqrt()) * eps_t  # Eqn 13 of DDIM (classifier-guidance paper showed the eqn can be used for forward process too)
+            sig_t = sampler.ddim_sigmas[tIdx]            # sigma_{t} (has already been scaled by ddim_eta)
+            x_dt = a_dt.sqrt() * x_t + ((1 - ab_dt - sig_t ** 2).sqrt() - (a_dt - ab_dt).sqrt()) * eps_t + sig_t * eps  # Eqn 12 of DDIM (classifier-guidance paper showed the eqn can be used for forward process too)
             x_t = x_dt
             if record_latents:
                 latentList.append(x_t.cpu())
     return x_t, latentList
 
-def noise_to_image(noise_batch, model, sampler, t_enc, cond, uncond, record_latents=False):
+def noise_to_image(noise_batch, model, sampler, t_enc, cond, uncond, cfg_scale, record_latents=False):
     # Initialize x_t and loop through prediction steps
-    condInput = cond if uncond is None else torch.cat([cond, uncond], dim=0)
-    nRepeat = 1 if uncond is None else 2
+    condInput = cond if uncond is None or cfg_scale == 1. else torch.cat([cond, uncond], dim=0)
     x_t = noise_batch
     latentList = [x_t.detach().cpu()] if record_latents else None
     with torch.no_grad():
         for tIdx in reversed(range(t_enc)):
             t = sampler.ddim_timesteps[tIdx]
-            eps_t = model.apply_model(x_t.repeat(nRepeat, 1, 1, 1), torch.ones(len(noise_batch)).cuda() * t, cond=condInput)
-            ab_t = sampler.ddim_alphas[tIdx]        # alpha_bar_t
+            if uncond is None or cfg_scale == 1.:
+                eps_t = model.apply_model(x_t, torch.full([len(x_t)], t, device=x_t.device), condInput)
+            else:
+                eps_t_cond, eps_t_uncond = model.apply_model(torch.cat([x_t] * 2), torch.full([2 * len(x_t)], t, device=x_t.device), condInput).chunk(2)
+                eps_t = eps_t_uncond + cfg_scale * (eps_t_cond - eps_t_uncond)
+            eps = torch.randn_like(eps_t)
+            ab_t = sampler.ddim_alphas[tIdx]        # alpha_bar_{t}
             ab_dt = sampler.ddim_alphas_prev[tIdx]  # alpha_bar_{t-dt}
             a_dt = ab_dt / ab_t                     # Product of alphas between alpha_{t} and alpha_{t-dt}
-            x_dt = a_dt.sqrt() * x_t + ((1 - ab_dt).sqrt() - (a_dt - ab_dt).sqrt()) * eps_t  # Eqn 13 of DDIM
+            sig_t = sampler.ddim_sigmas[tIdx]       # sigma_{t} (has already been scaled by ddim_eta)
+            x_dt = a_dt.sqrt() * x_t + ((1 - ab_dt - sig_t ** 2).sqrt() - (a_dt - ab_dt).sqrt()) * eps_t + sig_t * eps  # Eqn 12 of DDIM
             x_t = x_dt
             if record_latents:
                 latentList.append(x_t.cpu())
